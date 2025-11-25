0ab78ad344e5e7966e3a5a2a7a7dd741afe4623d
Choi Chan Hwan <africakokiri@gmail.com>
2025-11-25 12:20:45 +0900

    feat(ui): prompt 가운데 정렬

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,9 @@ import { Prompt } from "@/components/prompt";
 
 export default function page() {
   return (
-    <div className="relative h-screen p-8 pb-32 pl-72">
-      <div>asd</div>
-      <div className="fixed right-0 bottom-8 left-72 flex justify-center">
-        <div className="w-full max-w-3xl px-6">
-          <Prompt />
-        </div>
+    <div className="flex h-screen items-center justify-center">
+      <div className="w-full">
+        <Prompt />
       </div>
     </div>
   );

ff0122d0704eaf4e3148e9cf1d5bf3ddae7449e7
Choi Chan Hwan <africakokiri@gmail.com>
2025-12-02 14:05:03 +0900

    feat: auth-guard 컴포넌트 로그인, 회원가입 버튼 적용

--- src/components/_auth-guard.tsx
+++ src/components/_auth-guard.tsx
@@ -1,9 +1,39 @@
 "use client";
 
+import { Thread } from "@/components/thread/_thread";
+import { Button } from "@/ui/button";
+
+import { AssistantRuntimeProvider } from "@assistant-ui/react";
+import { AssistantChatTransport, useChatRuntime } from "@assistant-ui/react-ai-sdk";
 import { useUser } from "@clerk/nextjs";
 
 export default function AuthGuard() {
   const { isSignedIn } = useUser();
 
-  return <>{isSignedIn ? <div></div> : <div></div>}</>;
+  return <>{isSignedIn ? <div></div> : <NotSignedIn />}</>;
 }
+
+const NotSignedIn = () => {
+  const runtime = useChatRuntime({
+    transport: new AssistantChatTransport({
+      api: "/api/chat"
+    })
+  });
+  return (
+    <AssistantRuntimeProvider runtime={runtime}>
+      <div className="h-dvh">
+        <header className="fixed flex w-full justify-end gap-4 p-4">
+          <Button
+            size="sm"
+            variant="outline"
+          >
+            Sign in
+          </Button>
+          <Button size="sm">Sign up</Button>
+        </header>
+
+        <Thread />
+      </div>
+    </AssistantRuntimeProvider>
+  );
+};

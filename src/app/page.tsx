6f250cb323196a4d17391291a8ac1af2e7dd57c5
Choi Chan Hwan <africakokiri@gmail.com>
2025-12-02 13:50:01 +0900

    feat: auth-guard 컴포넌트 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,5 @@
-"use client";
+import AuthGuard from "@/components/_auth-guard";
 
-import { ThreadList } from "@/components/thread-list/_thread-list";
-import { Thread } from "@/components/thread/_thread";
-
-import { AssistantRuntimeProvider } from "@assistant-ui/react";
-import { AssistantChatTransport, useChatRuntime } from "@assistant-ui/react-ai-sdk";
-
-const MyApp = () => {
-  const runtime = useChatRuntime({
-    transport: new AssistantChatTransport({
-      api: "/api/chat"
-    })
-  });
-
-  return (
-    <AssistantRuntimeProvider runtime={runtime}>
-      <div>
-        <ThreadList />
-        <Thread />
-      </div>
-    </AssistantRuntimeProvider>
-  );
-};
-
-export default MyApp;
+export default function page() {
+  return <AuthGuard />;
+}

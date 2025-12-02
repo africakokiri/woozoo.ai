7727351629fef8a6cc11ef80cce7c21ac484d48d
Choi Chan Hwan <africakokiri@gmail.com>
2025-12-02 12:52:10 +0900

    chore: clerk provider 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { inter } from "@/styles/fonts";
 import "@/styles/globals.css";
 
+import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { ThemeProvider } from "next-themes";
 
@@ -18,20 +19,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      suppressHydrationWarning
-    >
-      <body className={`${inter.className} antialiased`}>
-        <ThemeProvider
-          enableColorScheme={false}
-          attribute="class"
-          disableTransitionOnChange
-          defaultTheme="system"
-        >
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html
+        lang="en"
+        suppressHydrationWarning
+      >
+        <body className={`${inter.className} antialiased`}>
+          <ThemeProvider
+            enableColorScheme={false}
+            attribute="class"
+            disableTransitionOnChange
+            defaultTheme="system"
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }

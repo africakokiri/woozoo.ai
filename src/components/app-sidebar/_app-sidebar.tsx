39d097b178f2806ad54ea9d50d85ff220c26a231
Choi Chan Hwan <africakokiri@gmail.com>
2025-12-06 08:40:39 +0900

    feat: app-sidebar-header 컴포넌트 적용

--- src/components/app-sidebar/_app-sidebar.tsx
+++ src/components/app-sidebar/_app-sidebar.tsx
@@ -1,12 +1,16 @@
 "use client";
 
+import { AppSidebarHeader } from "@/components/app-sidebar/app-sidebar-header";
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader } from "@/ui/sidebar";
 
 export default function AppSidebar() {
   return (
     <Sidebar>
-      <SidebarHeader></SidebarHeader>
-      <SidebarContent></SidebarContent>
+      <SidebarHeader>
+        <AppSidebarHeader />
+      </SidebarHeader>
+
+      <SidebarContent className="border-y"></SidebarContent>
       <SidebarFooter></SidebarFooter>
     </Sidebar>
   );

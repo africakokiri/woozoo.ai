7fca3ded5d9c65aaf3494342f3ffb80c99adf046
Choi Chan Hwan <africakokiri@gmail.com>
2025-12-06 11:28:36 +0900

    refactor: nav-main 컴포넌트 불필요한 코드 삭제

--- src/components/app-sidebar/nav-main.tsx
+++ src/components/app-sidebar/nav-main.tsx
@@ -1,23 +1,16 @@
 "use client";
 
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger
-} from "@/ui/dropdown-menu";
+import { DropdownMenu, DropdownMenuTrigger } from "@/ui/dropdown-menu";
 import {
   SidebarGroup,
-  SidebarGroupLabel,
   SidebarMenu,
   SidebarMenuAction,
   SidebarMenuButton,
-  SidebarMenuItem,
-  useSidebar
+  SidebarMenuItem
 } from "@/ui/sidebar";
 
-import { Folder, Forward, type LucideIcon, MoreHorizontal, Trash2 } from "lucide-react";
+import { type LucideIcon, MoreHorizontal } from "lucide-react";
+import Link from "next/link";
 
 export const NavMain = ({
   tools
@@ -28,8 +21,6 @@ export const NavMain = ({
     icon: LucideIcon;
   }[];
 }) => {
-  const { isMobile } = useSidebar();
-
   return (
     <SidebarGroup>
       <SidebarMenu>
@@ -39,10 +30,10 @@ export const NavMain = ({
               asChild
               tooltip={item.name}
             >
-              <a href={item.url}>
-                <item.icon />
+              <Link href={item.url}>
+                <item.icon strokeWidth={1.5} />
                 <span>{item.name}</span>
-              </a>
+              </Link>
             </SidebarMenuButton>
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
@@ -51,25 +42,6 @@ export const NavMain = ({
                   <span className="sr-only">More</span>
                 </SidebarMenuAction>
               </DropdownMenuTrigger>
-              <DropdownMenuContent
-                className="w-48 rounded-lg"
-                side={isMobile ? "bottom" : "right"}
-                align={isMobile ? "end" : "start"}
-              >
-                <DropdownMenuItem>
-                  <Folder className="text-muted-foreground" />
-                  <span>View Project</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  <Forward className="text-muted-foreground" />
-                  <span>Share Project</span>
-                </DropdownMenuItem>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>
-                  <Trash2 className="text-muted-foreground" />
-                  <span>Delete Project</span>
-                </DropdownMenuItem>
-              </DropdownMenuContent>
             </DropdownMenu>
           </SidebarMenuItem>
         ))}

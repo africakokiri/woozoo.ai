19d755ba5c4fad34294b19500abc27f2dfd1cac6
Choi Chan Hwan <africakokiri@gmail.com>
2025-12-06 13:25:08 +0900

    feat: nav-main 컴포넌트 적용

--- src/components/app-sidebar/nav-main.tsx
+++ src/components/app-sidebar/nav-main.tsx
@@ -1,13 +1,18 @@
 "use client";
 
+import { Button } from "@/ui/button";
+import { Card } from "@/ui/card";
 import { DropdownMenu, DropdownMenuTrigger } from "@/ui/dropdown-menu";
+import { Separator } from "@/ui/separator";
 import {
   SidebarGroup,
   SidebarMenu,
   SidebarMenuAction,
   SidebarMenuButton,
-  SidebarMenuItem
+  SidebarMenuItem,
+  useSidebar
 } from "@/ui/sidebar";
+import { cn } from "@/utils/shadcn/cn";
 
 import { type LucideIcon, MoreHorizontal } from "lucide-react";
 import Link from "next/link";
@@ -21,31 +26,68 @@ export const NavMain = ({
     icon: LucideIcon;
   }[];
 }) => {
+  const { open } = useSidebar();
+
   return (
-    <SidebarGroup>
-      <SidebarMenu>
-        {tools.map((item) => (
-          <SidebarMenuItem key={item.name}>
-            <SidebarMenuButton
-              asChild
-              tooltip={item.name}
-            >
-              <Link href={item.url}>
-                <item.icon strokeWidth={1.5} />
-                <span>{item.name}</span>
-              </Link>
-            </SidebarMenuButton>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <SidebarMenuAction showOnHover>
-                  <MoreHorizontal />
-                  <span className="sr-only">More</span>
-                </SidebarMenuAction>
-              </DropdownMenuTrigger>
-            </DropdownMenu>
+    <>
+      <SidebarGroup className={cn(!open && "hidden")}>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <Card className="rounded-3xl px-4 font-light shadow-none">
+              <div className="flex flex-1 items-center justify-between">
+                <div className="flex flex-col">
+                  <span className="text-sidebar-foreground/80 text-xs tracking-wider">CREDITS</span>
+                  <span className="text-sidebar-foreground mt-1 text-2xl">$24.80</span>
+                </div>
+
+                <Button size="sm">Recharge</Button>
+              </div>
+
+              <Separator />
+
+              <div className="space-y-4">
+                <div className="flex items-center justify-between">
+                  <span className="text-sidebar-foreground/80 text-xs tracking-wider">
+                    SESSION TOTAL
+                  </span>
+                  <span className="text-xl">1,322</span>
+                </div>
+
+                <div className="flex items-center justify-between">
+                  <span className="text-sidebar-foreground/80 text-xs tracking-wider">TOTAL COST</span>
+                  <span className="text-lg">$0.14</span>
+                </div>
+              </div>
+            </Card>
           </SidebarMenuItem>
-        ))}
-      </SidebarMenu>
-    </SidebarGroup>
+        </SidebarMenu>
+      </SidebarGroup>
+
+      <SidebarGroup>
+        <SidebarMenu>
+          {tools.map((item) => (
+            <SidebarMenuItem key={item.name}>
+              <SidebarMenuButton
+                asChild
+                tooltip={item.name}
+              >
+                <Link href={item.url}>
+                  <item.icon strokeWidth={1.5} />
+                  <span>{item.name}</span>
+                </Link>
+              </SidebarMenuButton>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <SidebarMenuAction showOnHover>
+                    <MoreHorizontal />
+                    <span className="sr-only">More</span>
+                  </SidebarMenuAction>
+                </DropdownMenuTrigger>
+              </DropdownMenu>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarGroup>
+    </>
   );
 };

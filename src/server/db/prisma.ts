ad6d4fafa6070a77187e9d7c3614e171ca0bed3b
Choi Chan Hwan <africakokiri@gmail.com>
2025-12-08 08:08:55 +0900

    chore: prisma createUser 함수 수정

--- src/server/db/prisma.ts
+++ src/server/db/prisma.ts
@@ -2,7 +2,7 @@
 
 import { prisma } from "@/libs/prisma/prisma";
 
-export const createUser = async (clerkUserId: string, email: string) => {
+export const createUser = async ({ clerkUserId, email }: { clerkUserId: string; email: string }) => {
   return await prisma.user.create({
     data: {
       id: clerkUserId,

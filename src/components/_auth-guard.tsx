4b61171b9dbf1c550e3bdf2e1071390fcab5f008
Choi Chan Hwan <africakokiri@gmail.com>
2025-12-07 11:33:06 +0900

    feat: auth-guard 컴포넌트 다크모드 스위치 추가

--- src/components/_auth-guard.tsx
+++ src/components/_auth-guard.tsx
@@ -8,6 +8,8 @@ import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from 
 import { AssistantRuntimeProvider } from "@assistant-ui/react";
 import { AssistantChatTransport, useChatRuntime } from "@assistant-ui/react-ai-sdk";
 import { SignInButton, useUser } from "@clerk/nextjs";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
 
 export default function AuthGuard() {
   const { isSignedIn, isLoaded } = useUser();
@@ -38,6 +40,8 @@ const NotSignedIn = () => {
     <AssistantRuntimeProvider runtime={runtime}>
       <div className="h-dvh">
         <header className="fixed z-10 flex w-full justify-end gap-4 p-4">
+          <DarkmodeSwitch />
+
           <Dialog>
             <DialogTrigger asChild>
               <SignInButton
@@ -65,5 +69,18 @@ const NotSignedIn = () => {
         <Thread />
       </div>
     </AssistantRuntimeProvider>
+  );
+};
+
+const DarkmodeSwitch = () => {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <Button
+      size="icon"
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+    >
+      {theme === "dark" ? <Sun /> : <Moon />}
+    </Button>
   );
 };

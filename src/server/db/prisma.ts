b2e0787fa891db35fc2f61bb525d96e84e3054de
Choi Chan Hwan <africakokiri@gmail.com>
2025-12-08 05:18:02 +0900

    feat: prisma createUser 함수 추가

---
+++ src/server/db/prisma.ts
@@ -0,0 +1,12 @@
+"use server";
+
+import { prisma } from "@/libs/prisma/prisma";
+
+export const createUser = async (clerkUserId: string, email: string) => {
+  return await prisma.user.create({
+    data: {
+      id: clerkUserId,
+      email
+    }
+  });
+};

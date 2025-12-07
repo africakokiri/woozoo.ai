e19084a0cc26ce35c58020ca5b77b71345e61d28
Choi Chan Hwan <africakokiri@gmail.com>
2025-12-07 09:39:42 +0900

    chore: app-sidebar 컴포넌트 motion 수정

--- src/components/app-sidebar/_app-sidebar.tsx
+++ src/components/app-sidebar/_app-sidebar.tsx
@@ -6,10 +6,15 @@ import { cn } from "@/utils/shadcn/cn";
 import { ChevronsLeft, ChevronsRight, Search, SquarePen } from "lucide-react";
 import { motion } from "motion/react";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function AppSidebar() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isHover, setIsHover] = useState(false);
+
+  useEffect(() => {
+    console.log(isHover);
+  }, [isHover]);
 
   return (
     <motion.aside
@@ -18,31 +23,44 @@ export default function AppSidebar() {
         width: isSidebarOpen ? 288 : 64
       }}
       transition={{ type: "spring", stiffness: 350, damping: 40, bounce: 0 }}
-      className={cn("bg-sidebar border-r", isSidebarOpen ? "w-72" : "w-16")}
+      className="bg-sidebar border-r"
     >
-      <header
-        className={cn(
-          "flex w-full items-center justify-between p-4",
-          !isSidebarOpen && "justify-center"
-        )}
-      >
-        <div className={cn("flex items-center gap-2", !isSidebarOpen && "hidden")}>
-          <Image
-            src="/icons/woozoo.svg"
-            alt=""
-            width={32}
-            height={32}
-            className="dark:invert"
-          />
+      <header className="flex w-full items-center justify-between p-4">
+        <div className="flex items-center gap-2">
+          {isHover ? (
+            <Button
+              variant="ghost"
+              className="flex h-8 w-8 items-center justify-center"
+              onMouseLeave={() => setIsHover(false)}
+              onClick={() => {
+                setIsSidebarOpen(!isSidebarOpen);
+                setIsHover(false);
+              }}
+            >
+              <ChevronsRight />
+            </Button>
+          ) : (
+            <Image
+              src="/icons/woozoo.svg"
+              alt=""
+              width={32}
+              height={32}
+              onMouseEnter={() => !isSidebarOpen && setIsHover(true)}
+              className="h-8! w-8! dark:invert"
+            />
+          )}
 
-          <span className="text-lg font-light">WooZoo</span>
+          <span className={cn("text-lg font-light", !isSidebarOpen && "hidden")}>WooZoo</span>
         </div>
 
         <Button
           variant="ghost"
-          onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+          onClick={() => {
+            setIsSidebarOpen(!isSidebarOpen);
+          }}
+          className={cn("h-8 w-8", !isSidebarOpen && "hidden")}
         >
-          {isSidebarOpen ? <ChevronsRight strokeWidth={1.5} /> : <ChevronsLeft strokeWidth={1.5} />}
+          {isSidebarOpen && <ChevronsLeft />}
         </Button>
       </header>
 
@@ -51,8 +69,7 @@ export default function AppSidebar() {
           {["New chats", "Search chats"].map((item, i) => (
             <li
               key={`${item}-${i}`}
-              className="flex w-full items-center gap-2 px-2 text-sm [&_svg]:h-5 [&_svg]:w-5
-[&_svg]:stroke-[1.5]"
+              className="flex w-full items-center gap-2 px-2 text-sm"
             >
               <Button
                 variant="ghost"

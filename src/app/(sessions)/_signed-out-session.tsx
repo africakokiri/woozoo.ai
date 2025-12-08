cbf7a473a9ed0148b65f9643530af72b0c9ba71e
Choi Chan Hwan <africakokiri@gmail.com>
2025-12-09 07:47:09 +0900

    feat: _signed-out-session 컴포넌트 Sign in 버튼 추가

--- src/app/(sessions)/_signed-out-session.tsx
+++ src/app/(sessions)/_signed-out-session.tsx
@@ -1,6 +1,42 @@
-// Route: /
-// Description: 로그인하지 않은 사용자가 보는 세션
+"use client";
+
+/**
+ * Route: /
+ * Description: 로그인하지 않은 사용자가 보는 세션
+ */
+import { Thread } from "@/components/assistant-ui/thread";
+import { Button } from "@/ui/button";
+
+import { AssistantRuntimeProvider } from "@assistant-ui/react";
+import { AssistantChatTransport, useChatRuntime } from "@assistant-ui/react-ai-sdk";
+import { SignInButton } from "@clerk/nextjs";
 
 export default function SignedOutSession() {
-  return <div></div>;
+  const runtime = useChatRuntime({
+    transport: new AssistantChatTransport({
+      api: "/api/chat"
+    })
+  });
+
+  return (
+    <AssistantRuntimeProvider runtime={runtime}>
+      <div className="h-dvh">
+        <header className="fixed z-10 flex w-full justify-end gap-4 p-4">
+          <SignInButton
+            mode="modal"
+            appearance={{
+              elements: {
+                modalBackdrop: "flex items-center!",
+                modalCloseButton: "ring-0!"
+              }
+            }}
+            forceRedirectUrl="/"
+          >
+            <Button>Sign in</Button>
+          </SignInButton>
+        </header>
+        <Thread />
+      </div>
+    </AssistantRuntimeProvider>
+  );
 }

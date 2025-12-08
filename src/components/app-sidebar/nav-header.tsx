99c92ed7b1f9209c7e2e661b09186395bf5c22c5
Choi Chan Hwan <africakokiri@gmail.com>
2025-12-08 10:33:05 +0900

    feat: nav-header 컴포넌트 좌상단 로고 클릭 시 페이지 /로 이동

--- src/components/app-sidebar/nav-header.tsx
+++ src/components/app-sidebar/nav-header.tsx
@@ -5,6 +5,7 @@ import { TooltipButton } from "@/ui/tooltip-button";
 import { ChevronsLeft, ChevronsRight } from "lucide-react";
 import { AnimatePresence, motion } from "motion/react";
 import Image from "next/image";
+import Link from "next/link";
 import { type Dispatch, type SetStateAction, useState } from "react";
 
 export const NavHeader = ({
@@ -33,28 +34,34 @@ export const NavHeader = ({
             <ChevronsRight aria-hidden={true} />
           </TooltipButton>
         ) : (
-          <Image
-            src="/icons/woozoo.svg"
-            alt="WooZoo logo"
-            width={32}
-            height={32}
-            onMouseEnter={() => !isSidebarOpen && setIsHover(true)}
-            className="h-8! w-8! dark:invert"
-          />
+          <Link href="/">
+            <Image
+              src="/icons/woozoo.svg"
+              alt="WooZoo logo"
+              width={32}
+              height={32}
+              onMouseEnter={() => !isSidebarOpen && setIsHover(true)}
+              className="h-8! w-8! dark:invert"
+            />
+          </Link>
         )}
 
         <AnimatePresence>
           {isSidebarOpen && (
-            <motion.span
+            <motion.div
               key="nav-header-logo-text"
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
               transition={{ duration: 0.2 }}
-              className="text-lg font-light"
             >
-              WooZoo
-            </motion.span>
+              <Link
+                href="/"
+                className="text-lg font-light"
+              >
+                WooZoo
+              </Link>
+            </motion.div>
           )}
         </AnimatePresence>
       </div>

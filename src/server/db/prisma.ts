1800a7f01e8f1245d9cf0b2204fb310dbbab8936
Choi Chan Hwan <africakokiri@gmail.com>
2025-12-08 13:48:29 +0900

    chore: zustand createNewChatSession 수정

--- src/server/db/prisma.ts
+++ src/server/db/prisma.ts
@@ -3,8 +3,6 @@
 import { prisma } from "@/libs/prisma/prisma";
 
 import { auth } from "@clerk/nextjs/server";
-import { randomUUID } from "crypto";
-import { redirect } from "next/navigation";
 
 export const createUser = async ({ clerkUserId, email }: { clerkUserId: string; email: string }) => {
   return await prisma.user.create({
@@ -15,19 +13,17 @@ export const createUser = async ({ clerkUserId, email }: { clerkUserId: string; 
   });
 };
 
-export const createNewChatSession = async () => {
-  const userId = await auth();
+export const createNewChatSession = async (publicId: string) => {
+  const { userId } = await auth();
 
   if (!userId) throw new Error("Unauthorized");
 
-  const session = await prisma.chatSession.create({
+  await prisma.chatSession.create({
     data: {
-      userId: userId.userId ?? "",
-      publicId: randomUUID(),
+      publicId,
+      userId,
       title: "New Chat",
       model: "gemini-flash-2.5"
     }
   });
-
-  redirect(`/chat/${session.publicId}`);
 };

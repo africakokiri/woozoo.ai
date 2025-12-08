6e89a772a5c95dc37bbda3d2ee9d873b22bc111a
Choi Chan Hwan <africakokiri@gmail.com>
2025-12-08 15:46:18 +0900

    feat: API Route 복구

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -6,12 +6,12 @@ export const maxDuration = 30;
 export async function POST(req: Request) {
   const { messages, model } = await req.json();
 
-  // const modelInstance = google("gemini-2.5-flash");
+  const modelInstance = google("gemini-2.5-flash");
 
-  // const result = streamText({
-  //   model: modelInstance,
-  //   messages: convertToModelMessages(messages)
-  // });
+  const result = streamText({
+    model: modelInstance,
+    messages: convertToModelMessages(messages)
+  });
 
-  // return result.toUIMessageStreamResponse();
+  return result.toUIMessageStreamResponse();
 }

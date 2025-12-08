1d4e3f6b5b924d3fb80969a7373ad5982b240f79
Choi Chan Hwan <africakokiri@gmail.com>
2025-12-09 07:44:55 +0900

    chore: _initial-session 컴포넌트 Sign out 버튼 임시 추가
    - signed out 세션을 구성하기 위해 임시 추가

--- src/app/(sessions)/_initial-session.tsx
+++ src/app/(sessions)/_initial-session.tsx
@@ -4,9 +4,14 @@
 "use client";
 
 import { Thread } from "@/components/assistant-ui/thread";
+import { Button } from "@/ui/button";
 
 import { AssistantRuntimeProvider } from "@assistant-ui/react";
 import { AssistantChatTransport, useChatRuntime } from "@assistant-ui/react-ai-sdk";
+import { SignOutButton } from "@clerk/nextjs";
+
+// Route: /
+// Description: 로그인 한 사용자가 보는 세션
 
 // Route: /
 // Description: 로그인 한 사용자가 보는 세션
@@ -20,6 +25,9 @@ export default function InitialSession() {
   return (
     <AssistantRuntimeProvider runtime={runtime}>
       <Thread />
+      <SignOutButton>
+        <Button>Sing out</Button>
+      </SignOutButton>
     </AssistantRuntimeProvider>
   );
 }

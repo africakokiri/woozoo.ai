85606ceceff61a4e2cc21810fe858e60b0a2872c
Choi Chan Hwan <africakokiri@gmail.com>
2025-12-08 15:04:24 +0900

    chore: /sessionPage 임시 저장

--- src/app/[chat]/[publicId]/page.tsx
+++ src/app/[chat]/[publicId]/page.tsx
@@ -1,5 +1,14 @@
 import SessionPage from "@/components/_session-page";
+import { getSessionByPublicId } from "@/server/db/prisma";
 
-export default function sessionPage() {
-  return <SessionPage />;
+export default async function sessionPage({ params }: { params: { publicId: string } }) {
+  const session = await getSessionByPublicId({
+    params
+  });
+
+  if (!session) {
+    return <div className="h-screen w-screen text-5xl">세션을 찾을 수 없습니다.</div>;
+  }
+
+  // return <SessionPage />;
 }

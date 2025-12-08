5e4dcae2b9f5d714f0fc8de983afe80f51ae5ce5
Choi Chan Hwan <africakokiri@gmail.com>
2025-12-08 15:37:31 +0900

    chore: start-new-chat 함수 uuid 생성 방식 변경

--- src/server/actions/start-new-chat.ts
+++ src/server/actions/start-new-chat.ts
@@ -3,13 +3,12 @@
 import { createNewChatSession } from "@/server/db/prisma";
 
 import { redirect } from "next/navigation";
+import { v4 as uuidv4 } from "uuid";
 
 export const startNewChat = async (formData: FormData) => {
-  "use server";
-
   const prompt = formData.get("prompt") as string;
 
-  const publicId = crypto.randomUUID();
+  const publicId = uuidv4();
   const model = "gemini-2.5-flash";
 
   await createNewChatSession({

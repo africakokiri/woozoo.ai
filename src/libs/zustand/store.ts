5eb4322440d39957b8f9bbd05a746c39bd63d6b2
Choi Chan Hwan <africakokiri@gmail.com>
2025-12-08 12:21:21 +0900

    feat: zustand useModelStore 추가

---
+++ src/libs/zustand/store.ts
@@ -0,0 +1,20 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+type ModelStore = {
+  model: string;
+  setModel: (model: string) => void;
+};
+
+export const useModelStore = create<ModelStore>()(
+  persist(
+    (set) => ({
+      model: "gemini-2.5-flash",
+
+      setModel: (model) => set({ model })
+    }),
+    {
+      name: "selected-model"
+    }
+  )
+);

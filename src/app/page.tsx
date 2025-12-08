1e1e6872b205fc4732ab9969617b1fe8d060d91c
Choi Chan Hwan <africakokiri@gmail.com>
2025-12-09 07:30:00 +0900

    feat: mainPage 로그인 여부로 세션 이동하는 로직 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+/**
+ * Route: /
+ * Description: 로그인 여부에 따라 세션 이동
+ */
 import InitialSession from "@/app/(sessions)/_initial-session";
+import SignedOutSession from "@/app/(sessions)/_signed-out-session";
 
-export default function mainPage() {
-  return (
-    <div className="h-dvh">
-      <InitialSession />
-    </div>
-  );
+import { auth } from "@clerk/nextjs/server";
+
+export default async function mainPage() {
+  const { isAuthenticated } = await auth();
+
+  return <>{isAuthenticated ? <InitialSession /> : <SignedOutSession />}</>;
 }

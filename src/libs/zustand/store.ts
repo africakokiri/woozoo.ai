0d6bbb61852b14b397893a19ebd42233b3c2fa5d
Choi Chan Hwan <africakokiri@gmail.com>
2025-12-10 06:10:36 +0900

    chore: zustand 모든 hook useGlobalConfigStore로 통합
    - 하나의 config 내에서 관리하기 위함

--- src/libs/zustand/store.ts
+++ src/libs/zustand/store.ts
@@ -1,34 +1,38 @@
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 
-type FirstRenderStore = {
-  isFirstRender: boolean;
-
-  finishFirstRender: () => void;
-};
-
-export const useFirstRenderStore = create<FirstRenderStore>((set) => ({
-  isFirstRender: true,
-
-  finishFirstRender: () => set(() => ({ isFirstRender: false }))
-}));
-
 type GlobalConfigStore = {
-  isSidebarOpen: boolean;
+  isFirstRender: boolean;
+  finishFirstRender: () => void;
 
+  isHydrated: boolean;
+  setIsHydrated: (state: boolean) => void;
+
+  isSidebarOpen: boolean;
   setIsSidebarOpen: (isSidebarOpen: boolean) => void;
 };
 
 export const useGlobalConfigStore = create<GlobalConfigStore>()(
   persist(
     (set, get) => ({
-      isSidebarOpen: false,
+      isFirstRender: true,
+      finishFirstRender: () => set(() => ({ isFirstRender: false })),
 
+      isHydrated: false,
+      setIsHydrated: () => set({ isHydrated: !get().isHydrated }),
+
+      isSidebarOpen: false,
       setIsSidebarOpen: () => set({ isSidebarOpen: !get().isSidebarOpen })
     }),
     {
       name: "open-sidebar",
-      storage: createJSONStorage(() => localStorage)
+      storage: createJSONStorage(() => localStorage),
+
+      partialize: (state) => ({
+        isSidebarOpen: state.isSidebarOpen
+      }),
+
+      onRehydrateStorage: (state) => () => state.setIsHydrated(true)
     }
   )
 );

ffc8994e0d9eccacab538fe1705e9811fc08e7fe
Choi Chan Hwan <africakokiri@gmail.com>
2025-12-09 10:35:53 +0900

    feat: sidebar-header 컴포넌트 a11y 준수

--- src/components/sidebar/sidebar-header.tsx
+++ src/components/sidebar/sidebar-header.tsx
@@ -1,12 +1,10 @@
-import { TooltipIconButton } from "@/components/assistant-ui/tooltip-icon-button";
 import { Button } from "@/ui/button";
-import { cn } from "@/utils/tailwind/cn";
 
 import { ChevronsLeft, ChevronsRight } from "lucide-react";
 import { AnimatePresence, type Variants, motion } from "motion/react";
 import Image from "next/image";
 import Link from "next/link";
-import { type Dispatch, type SetStateAction, useState } from "react";
+import { type Dispatch, type SetStateAction } from "react";
 
 const titleVariants: Variants = {
   hidden: { opacity: 0 },
@@ -23,54 +21,36 @@ export const SidebarHeader = ({
   isSidebarOpen: boolean;
   setIsSidebarOpen: Dispatch<SetStateAction<boolean>>;
 }) => {
-  const [isHover, setIsHover] = useState(false);
-
   return (
-    <div className="flex h-16 items-center">
+    <div className="space-y-4">
       <div className="flex h-12 flex-1 items-center justify-between">
-        {isHover ? (
-          <Button
-            aria-label="Open sidebar"
-            variant="ghost"
-            onMouseLeave={() => setIsHover(false)}
-            className="flex h-12 w-12 items-center justify-center"
-            onClick={() => {
-              setIsSidebarOpen(!isSidebarOpen);
-              setIsHover(false);
-            }}
-          >
-            <ChevronsRight aria-hidden={true} />
-          </Button>
-        ) : (
-          <Link
-            href="/"
-            className="flex h-12 min-w-fit items-center gap-2 p-0"
-          >
-            <Image
-              src="/icons/main.svg"
-              alt="WooZoo logo"
-              width={48}
-              height={48}
-              onMouseEnter={() => !isSidebarOpen && setIsHover(true)}
-              className="size-12 dark:invert"
-            />
-            <AnimatePresence>
-              {isSidebarOpen && (
-                <motion.span
-                  key="sidebar-title"
-                  variants={titleVariants}
-                  initial="hidden"
-                  animate="visible"
-                  exit="hidden"
-                  aria-hidden={!isSidebarOpen}
-                  className="text-sidebar-foreground text-2xl font-light tracking-tight"
-                >
-                  WooZoo
-                </motion.span>
-              )}
-            </AnimatePresence>
-          </Link>
-        )}
+        <Link
+          href="/"
+          className="fvis flex items-center gap-2"
+        >
+          <Image
+            src="/icons/main.svg"
+            alt="WooZoo logo"
+            width={48}
+            height={48}
+            className="size-12 dark:invert"
+          />
+          <AnimatePresence>
+            {isSidebarOpen && (
+              <motion.h1
+                key="sidebar-title"
+                variants={titleVariants}
+                initial="hidden"
+                animate="visible"
+                exit="hidden"
+                aria-hidden={!isSidebarOpen}
+                className="text-sidebar-foreground text-2xl font-light tracking-tight"
+              >
+                WooZoo
+              </motion.h1>
+            )}
+          </AnimatePresence>
+        </Link>
 
         {isSidebarOpen && (
           <Button
@@ -83,6 +63,17 @@ export const SidebarHeader = ({
           </Button>
         )}
       </div>
+
+      {!isSidebarOpen && (
+        <Button
+          aria-label="Close sidebar"
+          variant="ghost"
+          className="flex h-12 w-12 items-center justify-center"
+          onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+        >
+          <ChevronsRight />
+        </Button>
+      )}
     </div>
   );
 };

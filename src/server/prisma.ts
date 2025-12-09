a56b1fda5031ef751341f5e99669ab1da7cd5c8a
Choi Chan Hwan <africakokiri@gmail.com>
2025-12-10 08:23:30 +0900

    feat: prisma createNewChatSession 함수 추가

--- src/server/prisma.ts
+++ src/server/prisma.ts
@@ -2,7 +2,9 @@
 
 import { prisma } from "@/libs/prisma/prisma";
 
-import { currentUser } from "@clerk/nextjs/server";
+import { google } from "@ai-sdk/google";
+import { auth, currentUser } from "@clerk/nextjs/server";
+import { generateText } from "ai";
 
 export const createUserIfNotExist = async () => {
   const user = await currentUser();
@@ -16,5 +18,39 @@ export const createUserIfNotExist = async () => {
       email: user.emailAddresses[0].emailAddress
     },
     update: {}
+  });
+};
+
+export const createNewChatSession = async ({
+  publicId,
+  prompt,
+  model
+}: {
+  publicId: string;
+  prompt: string;
+  model: string;
+}) => {
+  const { userId } = await auth();
+
+  if (!userId) throw new Error("Unauthorized");
+
+  const { text: title } = await generateText({
+    model: google("gemini-2.5.-flash"),
+    prompt: `
+      아래 유저의 입력을 보고, 채팅 세션 제목을 10자 내로 간단하게 만들어줘.
+      핵심만 요약할 것.
+      
+      사용자 입력:
+      """${prompt}"""
+    `
+  });
+
+  await prisma.chatSession.create({
+    data: {
+      publicId,
+      userId,
+      title: title ?? "New Chat",
+      model
+    }
   });
 };

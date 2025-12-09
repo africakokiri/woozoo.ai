1c00bc89f165f050a5f3c83ec8a4592d67f1a7aa
Choi Chan Hwan <africakokiri@gmail.com>
2025-12-09 10:53:15 +0900

    style: sidebar 컴포넌트 스타일 수정
    - header, nav 컴포넌트 간 간격 수정

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -2,6 +2,7 @@
 
 import { SidebarHeader } from "@/components/sidebar/sidebar-header";
 import { SidebarNav } from "@/components/sidebar/sidebar-nav";
+import { cn } from "@/utils/tailwind/cn";
 
 import { motion } from "motion/react";
 import { useState } from "react";
@@ -16,7 +17,7 @@ export default function Sidebar() {
       transition={{ type: "spring", stiffness: 350, damping: 40, bounce: 0 }}
       className="bg-sidebar sticky top-0 left-0 h-screen shrink-0 border-r"
     >
-      <header className="p-4">
+      <header className={cn("px-4 pt-4 pb-1", isSidebarOpen && "pb-4")}>
         <SidebarHeader
           isSidebarOpen={isSidebarOpen}
           setIsSidebarOpen={setIsSidebarOpen}

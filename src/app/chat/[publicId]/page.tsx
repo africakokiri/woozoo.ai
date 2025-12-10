42aff5ec40aa2fb0e85f507b167f46941947b9bc
Choi Chan Hwan <africakokiri@gmail.com>
2025-12-10 15:31:07 +0900

    chore: /chat/[publicId] route 삭제

--- src/app/chat/[publicId]/page.tsx
+++
@@ -1,7 +0,0 @@
-import ChatSession from "@/app/chat/[publicId]/(sessions)/chat-session";
-
-export default async function chatPage(props: { params: Promise<{ publicId: string }> }) {
-  const publicId = await props.params;
-
-  return <ChatSession />;
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 import { createUserIfNotExist } from "@/utils/server/prisma";
 
-import { auth } from "@clerk/nextjs/server";
-
 export default async function mainPage() {
-  const { isAuthenticated } = await auth();
-
   await createUserIfNotExist();
 }

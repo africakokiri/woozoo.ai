c70c8e04866af0812f7b5c0178c047945ef2cdd2
Choi Chan Hwan <africakokiri@gmail.com>
2025-12-10 15:17:34 +0900

    feat: thread 컴포넌트 지연 로딩

--- src/context/thread-renderer.tsx
+++ src/context/thread-renderer.tsx
@@ -2,11 +2,16 @@
 
 import { Thread } from "@/components/assistant-ui/thread";
 import { useChatRuntimeInstance } from "@/context/chat-runtime-provider";
+import { useGlobalConfigStore } from "@/context/store";
 
 import { AssistantRuntimeProvider } from "@assistant-ui/react";
 
 export default function ThreadRenderer() {
+  const { isSidebarRendered } = useGlobalConfigStore();
+
   const runtime = useChatRuntimeInstance();
+
+  if (!isSidebarRendered) return null;
 
   return (
     <AssistantRuntimeProvider runtime={runtime}>

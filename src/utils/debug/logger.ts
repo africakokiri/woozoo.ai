b327118e15b4eea292d8fc68b5a4ce70609c2408
Choi Chan Hwan <africakokiri@gmail.com>
2025-12-10 11:53:13 +0900

    fix: debugger logger 수정
    - string 이외에는 undefined가 출력되던 현상 수정

--- src/utils/debug/logger.ts
+++ src/utils/debug/logger.ts
@@ -3,20 +3,24 @@
 import chalk from "chalk";
 
 type Logger = {
-  message: string;
+  message: unknown | string;
   status?: "PASS" | "WARNING" | "ERROR";
 };
 
 const logger = ({ message, status = "PASS" }: Logger) => {
   if (process.env.PRODUCTION === "true") return;
 
+  // switch 문 내부의 'return'을 제거합니다.
   switch (status) {
     case "PASS":
-      return console.log(`${chalk.bgHex("#00c950").black.bold(" PASS ")}${" " + message}`);
+      console.log(`${chalk.bgHex("#00c950").black.bold(" PASS ")}${" " + message}`);
+      break; // switch 문 종료를 위해 break를 사용합니다.
     case "WARNING":
-      return console.log(`${chalk.bgHex("#ffdf20").black.bold(" WARNING ")}${" " + message}`);
+      console.log(`${chalk.bgHex("#ffdf20").black.bold(" WARNING ")}${" " + message}`);
+      break;
     case "ERROR":
-      return console.log(`${chalk.bgHex("#fb2c36").hex("#f5f5f5").bold(" ERROR ")}${" " + message}`);
+      console.log(`${chalk.bgHex("#fb2c36").hex("#f5f5f5").bold(" ERROR ")}${" " + message}`);
+      break;
   }
 };
 

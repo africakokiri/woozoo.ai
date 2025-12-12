b6ac777ecf7a130b15f5b767ead41c262c1b0a74
Choi Chan Hwan <africakokiri@gmail.com>
2025-12-12 10:34:12 +0900

    feat: / 페이지 session 조건부 렌더링 적용
    - signedIn, signedOut 구분

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+import Session from "@/components/session";
 import { createUserIfNotExist } from "@/utils/server/prisma";
 
+import { auth } from "@clerk/nextjs/server";
+
 export default async function mainPage() {
+  const { isAuthenticated } = await auth();
+
   await createUserIfNotExist();
+
+  return <Session isAuthenticated={isAuthenticated} />;
 }

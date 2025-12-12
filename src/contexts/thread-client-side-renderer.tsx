810a057835d6fff9001d5ac1d7baf07720d8a0a0
Choi Chan Hwan <africakokiri@gmail.com>
2025-12-12 17:58:58 +0900

    feat: 뒤로가기 또는 / 경로로 이동 시 스레드 제거

--- src/contexts/thread-client-side-renderer.tsx
+++ src/contexts/thread-client-side-renderer.tsx
@@ -4,7 +4,9 @@ import { Thread } from "@/components/assistant-ui/thread";
 import { useChatRuntimeInstance } from "@/contexts/chat-runtime-context";
 import { useGlobalConfigStore } from "@/utils/zustand/use-global-config";
 
-import { AssistantRuntimeProvider } from "@assistant-ui/react";
+import { AssistantRuntimeProvider, useAssistantApi } from "@assistant-ui/react";
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
 
 export default function ThreadClientSideRenderer() {
   const { isSidebarRendered } = useGlobalConfigStore();
@@ -15,6 +17,22 @@ export default function ThreadClientSideRenderer() {
   return (
     <AssistantRuntimeProvider runtime={runtime}>
       <Thread />
+      <ThreadClientSideRendererSwitchThread />
     </AssistantRuntimeProvider>
   );
 }
+
+const ThreadClientSideRendererSwitchThread = () => {
+  const pathname = usePathname();
+  const api = useAssistantApi();
+
+  useEffect(() => {
+    if (pathname === "/") {
+      setTimeout(() => {
+        api.threads().switchToNewThread();
+      }, 0);
+    }
+  }, [pathname, api]);
+
+  return null;
+};

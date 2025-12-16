523a49c95921eca048238bd0fff00246478a26c0
Choi Chan Hwan <africakokiri@gmail.com>
2025-12-17 05:44:28 +0900

    feat: sidebar user 적용

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,15 +1,51 @@
 "use client";
 
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle
+} from "@/ui/alert-dialog";
+import { Avatar, AvatarFallback, AvatarImage } from "@/ui/avatar";
 import { Button } from "@/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger
+} from "@/ui/dropdown-menu";
 import { cn } from "@/utils/tailwind/cn";
 import { useGlobalConfigStore } from "@/utils/zustand/use-global-config-store";
 import { useSidebarStore } from "@/utils/zustand/use-sidebar-store";
 
-import { ChevronsLeft, ChevronsRight, Search, SquarePen } from "lucide-react";
+import { SignOutButton, useUser } from "@clerk/nextjs";
+import {
+  BadgeCheck,
+  ChevronsLeft,
+  ChevronsRight,
+  ChevronsUpDown,
+  CreditCard,
+  LogOut,
+  Search,
+  SquarePen,
+  SunMoon
+} from "lucide-react";
 import { AnimatePresence, type Variants, motion } from "motion/react";
+import { useTheme } from "next-themes";
 import Image from "next/image";
 import Link from "next/link";
-import { useEffect } from "react";
+import { type Dispatch, type SetStateAction, useEffect, useState } from "react";
 
 const motionVars: Variants = {
   hidden: { opacity: 0, transition: { duration: 0.1 } },
@@ -36,9 +72,9 @@ const Sidebar = () => {
   return (
     <motion.aside
       initial={false}
-      animate={{ width: isOpen ? 289 : 81 }}
+      animate={{ width: isOpen ? 288 : 80 }}
       transition={{ type: "spring", stiffness: 350, damping: 40, bounce: 0 }}
-      className="bg-sidebar sticky top-0 left-0 h-screen border-r"
+      className="bg-sidebar border-r-sidebar-border sticky top-0 left-0 flex h-screen shrink-0 flex-col"
     >
       <header className="p-4">
         <Sidebar.Header />
@@ -47,6 +83,10 @@ const Sidebar = () => {
       <nav className="px-4">
         <Sidebar.Nav />
       </nav>
+
+      <footer className="mt-auto p-4">
+        <Sidebar.User />
+      </footer>
     </motion.aside>
   );
 };
@@ -153,7 +193,162 @@ const Nav = () => {
   );
 };
 
-const User = () => {};
+export const User = () => {
+  const [isLogoutDialogOpen, setIsLogoutDialogOpen] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger
+          className="w-full"
+          asChild
+        >
+          <Button
+            variant="ghost"
+            className="h-12 px-0! py-6!"
+            aria-label="Open user account menu"
+          >
+            <UserProfile />
+          </Button>
+        </DropdownMenuTrigger>
+
+        <DropdownMenuContent
+          className="w-(--radix-dropdown-menu-trigger-width) max-w-42 overflow-hidden rounded-lg"
+          align="end"
+          side="right"
+          sideOffset={4}
+        >
+          <DropdownMenuGroup>
+            <DropdownMenuSub>
+              <DropdownMenuSubTrigger>
+                <SunMoon />
+                Appearance
+              </DropdownMenuSubTrigger>
+
+              <DropdownMenuSubContent
+                className="w-40"
+                sideOffset={4}
+              >
+                <DropdownMenuRadioGroup
+                  value={theme}
+                  onValueChange={setTheme}
+                >
+                  <DropdownMenuRadioItem value="system">System</DropdownMenuRadioItem>
+                  <DropdownMenuRadioItem value="dark">Dark</DropdownMenuRadioItem>
+                  <DropdownMenuRadioItem value="light">Light</DropdownMenuRadioItem>
+                </DropdownMenuRadioGroup>
+              </DropdownMenuSubContent>
+            </DropdownMenuSub>
+
+            <DropdownMenuItem>
+              <BadgeCheck />
+              Account
+            </DropdownMenuItem>
+
+            <DropdownMenuItem>
+              <CreditCard />
+              Billing
+            </DropdownMenuItem>
+
+            <DropdownMenuSeparator />
+
+            <DropdownMenuItem onClick={() => setIsLogoutDialogOpen(true)}>
+              <LogOut />
+              Sign out
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <AlertComponent
+        isLogoutDialogOpen={isLogoutDialogOpen}
+        setIsLogoutDialogOpen={setIsLogoutDialogOpen}
+      />
+    </>
+  );
+};
+
+const UserProfile = () => {
+  const { isOpen } = useSidebarStore();
+  const { isFirstRender } = useGlobalConfigStore();
+  const { user } = useUser();
+
+  return (
+    <div className="flex w-full items-center gap-2 p-2.5">
+      <Avatar className="h-7 w-7 rounded-full">
+        <AvatarImage
+          src={user?.imageUrl}
+          alt="Profile image"
+        />
+        <AvatarFallback className="rounded-lg">
+          {user?.firstName &&
+            user.firstName
+              .split(" ")
+              .map((word) => word[0])
+              .join("")}
+        </AvatarFallback>
+      </Avatar>
+
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            key="sidebar-user"
+            variants={motionVars}
+            initial={isFirstRender ? false : "hidden"}
+            animate="visible"
+            exit="hidden"
+            className="flex flex-1 items-center"
+          >
+            <div className="flex flex-col items-start justify-between">
+              <span className="text-sm font-medium">{user?.firstName}</span>
+              <span className="text-xs">{user?.emailAddresses[0].emailAddress}</span>
+            </div>
+
+            <ChevronsUpDown
+              className="ml-auto"
+              aria-hidden="true"
+              focusable="false"
+            />
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
+  );
+};
+
+const AlertComponent = ({
+  isLogoutDialogOpen,
+  setIsLogoutDialogOpen
+}: {
+  isLogoutDialogOpen: boolean;
+  setIsLogoutDialogOpen: Dispatch<SetStateAction<boolean>>;
+}) => {
+  return (
+    <AlertDialog
+      open={isLogoutDialogOpen}
+      onOpenChange={setIsLogoutDialogOpen}
+    >
+      <AlertDialogContent className="space-y-4">
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you sure you want to sign out?</AlertDialogTitle>
+          <AlertDialogDescription>
+            You will need to sign in again to continue using WooZoo.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+
+          <SignOutButton>
+            <AlertDialogAction className="bg-destructive hover:bg-destructive/50">
+              Sign out
+            </AlertDialogAction>
+          </SignOutButton>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
 
 Sidebar.Header = Header;
 Sidebar.Nav = Nav;

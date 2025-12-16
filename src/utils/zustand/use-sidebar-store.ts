e3d1904c745909059da3386e6e5f71005fcb70a2
Choi Chan Hwan <africakokiri@gmail.com>
2025-12-16 13:07:15 +0900

    feat: useSidebarStore sidebar render 여부 확인하는 로직 추가
    - main 콘텐츠 추가 시 lazy loading 사용하기 위해 추가

--- src/utils/zustand/use-sidebar-store.ts
+++ src/utils/zustand/use-sidebar-store.ts
@@ -4,13 +4,19 @@ import { createJSONStorage, persist } from "zustand/middleware";
 type SidebarStore = {
   isOpen: boolean;
   toggle: () => void;
+
+  isSidebarRendered: boolean;
+  setIsSidebarRendered: () => void;
 };
 
 export const useSidebarStore = create<SidebarStore>()(
   persist(
     (set, get) => ({
       isOpen: true,
-      toggle: () => set({ isOpen: !get().isOpen })
+      toggle: () => set({ isOpen: !get().isOpen }),
+
+      isSidebarRendered: false,
+      setIsSidebarRendered: () => set({ isSidebarRendered: true })
     }),
     {
       name: "sidebar",

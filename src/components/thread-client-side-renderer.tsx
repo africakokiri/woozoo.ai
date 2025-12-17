a926e36ff130995069a7228ccc4ff5dfaea29f12
Choi Chan Hwan <africakokiri@gmail.com>
2025-12-17 11:24:58 +0900

    chore: thread runtime 변경

--- src/components/thread-client-side-renderer.tsx
+++ src/components/thread-client-side-renderer.tsx
@@ -1,13 +1,12 @@
 "use client";
 
 import { Thread } from "@/components/assistant-ui/thread";
-import { useChatRuntimeInstance } from "@/contexts/chat-runtime-context";
 import { Button } from "@/ui/button";
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/ui/dialog";
 import { cn } from "@/utils/tailwind/cn";
 import { useSidebarStore } from "@/utils/zustand/use-sidebar-store";
 
-import { AssistantRuntimeProvider, useAssistantApi } from "@assistant-ui/react";
+import { useAssistantApi } from "@assistant-ui/react";
 import { SignInButton } from "@clerk/nextjs";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
@@ -16,52 +15,49 @@ import { useEffect } from "react";
 
 export default function ThreadClientSideRenderer({ isAuthenticated }: { isAuthenticated: boolean }) {
   const { isSidebarRendered } = useSidebarStore();
-  const runtime = useChatRuntimeInstance();
 
   const { theme, setTheme } = useTheme();
 
   if (!isSidebarRendered) return null;
 
   return (
-    <AssistantRuntimeProvider runtime={runtime}>
-      <div className="h-dvh">
-        <header
-          className={cn("fixed z-10 flex w-full justify-end gap-4 p-4", isAuthenticated && "hidden")}
+    <div className="h-dvh">
+      <header
+        className={cn("fixed z-10 flex w-full justify-end gap-4 p-4", isAuthenticated && "hidden")}
+      >
+        <Button
+          size="icon"
+          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
         >
-          <Button
-            size="icon"
-            onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-          >
-            {theme === "dark" ? <Sun /> : <Moon />}
-          </Button>
-          <Dialog>
-            <DialogTrigger asChild>
-              <SignInButton
-                mode="modal"
-                appearance={{
-                  elements: {
-                    modalBackdrop: "flex items-center!",
-                    modalCloseButton: "ring-0!"
-                  }
-                }}
-                forceRedirectUrl="/"
-              >
-                <Button>Sign in</Button>
-              </SignInButton>
-            </DialogTrigger>
+          {theme === "dark" ? <Sun /> : <Moon />}
+        </Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <SignInButton
+              mode="modal"
+              appearance={{
+                elements: {
+                  modalBackdrop: "flex items-center!",
+                  modalCloseButton: "ring-0!"
+                }
+              }}
+              forceRedirectUrl="/"
+            >
+              <Button>Sign in</Button>
+            </SignInButton>
+          </DialogTrigger>
 
-            <DialogContent>
-              <DialogHeader className="sr-only">
-                <DialogTitle>Sign in</DialogTitle>
-              </DialogHeader>
-            </DialogContent>
-          </Dialog>
-        </header>
+          <DialogContent>
+            <DialogHeader className="sr-only">
+              <DialogTitle>Sign in</DialogTitle>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
+      </header>
 
-        <Thread />
-        <ThreadClientSideRendererThreadSwitcher />
-      </div>
-    </AssistantRuntimeProvider>
+      <Thread />
+      <ThreadClientSideRendererThreadSwitcher />
+    </div>
   );
 }
 

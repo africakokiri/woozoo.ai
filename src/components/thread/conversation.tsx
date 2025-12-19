f98fdf81d431efbc325d5ba49b1d9189cb9a79a3
Choi Chan Hwan <africakokiri@gmail.com>
2025-12-20 07:11:44 +0900

    feat: @ai-sdk/rsc 관련 코드 전부 삭제

--- src/components/thread/conversation.tsx
+++ src/components/thread/conversation.tsx
@@ -1,28 +1,9 @@
 "use client";
 
-import { generateStreamValue } from "@/utils/server/generate-stream-value";
 import { useGlobalDataStore } from "@/utils/zustand/store";
 
-import { readStreamableValue } from "@ai-sdk/rsc";
-import { useEffect, useState } from "react";
-
 export const Conversation = () => {
-  const [generation, setGeneration] = useState<string>("");
   const { prompt } = useGlobalDataStore();
 
-  useEffect(() => {
-    (async () => {
-      const { output } = await generateStreamValue(prompt);
-
-      for await (const delta of readStreamableValue(output)) {
-        setGeneration((currentGeneration) => `${currentGeneration}${delta}`);
-      }
-    })();
-  }, [prompt]);
-
-  return (
-    <div>
-      <pre>{generation}</pre>
-    </div>
-  );
+  return <div></div>;
 };

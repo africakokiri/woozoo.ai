e8dd30edf4446bba132fdc3324fe95271a3ad79e
Choi Chan Hwan <africakokiri@gmail.com>
2025-12-20 07:25:22 +0900

    feat: conversation 컴포넌트 기본적인 렌더링

--- src/components/thread/conversation.tsx
+++ src/components/thread/conversation.tsx
@@ -1,9 +1,42 @@
 "use client";
 
+import { cn } from "@/utils/shadcn/cn";
 import { useGlobalDataStore } from "@/utils/zustand/store";
+
+import { useChat } from "@ai-sdk/react";
+import { DefaultChatTransport } from "ai";
+import { useEffect } from "react";
 
 export const Conversation = () => {
   const { prompt } = useGlobalDataStore();
+  const { messages, sendMessage, status } = useChat({
+    transport: new DefaultChatTransport({
+      api: "/api/chat" // 기본값도 /api/chat
+    })
+  });
 
-  return <div></div>;
+  useEffect(() => {
+    sendMessage({ text: prompt });
+  }, [prompt]);
+
+  return (
+    <div className="space-y-10">
+      {messages.map((m) => {
+        return (
+          <div key={m.id}>
+            {m.parts.map((part, index) =>
+              part.type === "text" && part.text !== "" ? (
+                <div
+                  key={index}
+                  className={cn("flex w-full", m.role === "user" ? "justify-end" : "justify-start")}
+                >
+                  {part.text}
+                </div>
+              ) : null
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
 };

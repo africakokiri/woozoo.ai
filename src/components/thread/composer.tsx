af948c43a7c734f7ab92a2943b8033ec09b2b0b3
Choi Chan Hwan <africakokiri@gmail.com>
2025-12-20 08:08:17 +0900

    style: composer 컴포넌트 스타일 변경
    - min-h-fit으로 변경

--- src/components/thread/composer.tsx
+++ src/components/thread/composer.tsx
@@ -29,8 +29,8 @@ export const Composer = () => {
     <form
       className="border-input placeholder:text-muted-foreground focus-within:border-ring
 focus-within:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40
-aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-16 w-full resize-none
-flex-col rounded-3xl border bg-transparent text-base shadow-xs transition-[color,box-shadow] outline-none
+aria-invalid:border-destructive dark:bg-input/30 bg-card flex field-sizing-content min-h-fit w-full
+resize-none flex-col rounded-3xl border shadow-xs transition-[color,box-shadow] outline-none
 focus-within:shadow-xl focus-within:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50
 md:text-sm"
       onSubmit={handleOnSubmit}
@@ -38,6 +38,7 @@ md:text-sm"
     >
       <Textarea
         className="h-full w-full resize-none border-none px-4 pt-4 outline-none"
+        placeholder="Ask me anything..."
         minRows={2}
         maxRows={8}
         value={prompt}

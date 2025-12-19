f2ce1479a3012ad773c815b60c79d8c3de031f8d
Choi Chan Hwan <africakokiri@gmail.com>
2025-12-19 14:29:57 +0900

    style: composer 컴포넌트 스타일 변경
    - buttons absolute 방식에서 block 방식으로 변경

--- src/components/thread/composer.tsx
+++ src/components/thread/composer.tsx
@@ -22,40 +22,37 @@ export const Composer = () => {
 
   return (
     <form
-      className="relative"
+      className="border-input placeholder:text-muted-foreground focus-within:border-ring
+focus-within:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40
+aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-16 w-full resize-none
+flex-col rounded-3xl border bg-transparent text-base shadow-xs transition-[color,box-shadow] outline-none
+focus-within:shadow-xl focus-within:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50
+md:text-sm"
       onSubmit={handleOnSubmit}
       ref={formRef}
     >
       <Textarea
-        className="border-input placeholder:text-muted-foreground focus-visible:border-ring
-focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40
-aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-16 w-full resize-none
-rounded-3xl border bg-transparent px-4 pt-4 pb-12 text-base shadow-xs transition-[color,box-shadow]
-outline-none focus-visible:shadow-xl focus-visible:ring-[3px] disabled:cursor-not-allowed
-disabled:opacity-50 md:text-sm"
+        className="h-full w-full resize-none border-none px-4 pt-4 outline-none"
         minRows={2}
         maxRows={8}
         value={prompt}
         ref={textareaRef}
         onChange={(e) => setPrompt(e.target.value)}
         onKeyDown={(e) => {
-          // Enter + Shift일 때 아무런 동작하지 않음
+          // Enter + Shift일 때 아무런 동작하지 않는다.
           if (e.key === "Enter" && !e.shiftKey) e.preventDefault();
 
-          // 마지막 글자가 폼 전송 때 포함되는 것 방지
+          // 마지막 글자가 폼 전송 때 포함되는 것을 방지한다.
           if (e.nativeEvent.isComposing) return;
 
-          // Enter + prompt.length > 0일 때 폼 제출
+          // Enter + prompt.length > 0일 때 폼을 제출한다.
           if (formRef.current && e.key === "Enter" && !e.shiftKey && prompt.trim().length > 0) {
             (formRef.current as HTMLFormElement).requestSubmit();
           }
         }}
       />
 
-      <div
-        className="absolute bottom-0 flex w-full justify-between p-2 [&_button]:size-8
-[&_button]:rounded-full"
-      >
+      <div className="flex w-full justify-between p-2 [&_button]:size-8 [&_button]:rounded-full">
         <div className="space-x-2">
           <Button
             variant="ghost"

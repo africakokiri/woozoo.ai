e8af8de4700457a17972b3125f8e53ac32867cdd
Choi Chan Hwan <africakokiri@gmail.com>
2026-01-07 18:18:20 +0900

    feat: nav 컴포넌트 new chat, search chats 버튼 추가
    - 기능 구현 X

--- src/widgets/sidebar/ui/nav.tsx
+++ src/widgets/sidebar/ui/nav.tsx
@@ -1,5 +1,40 @@
 "use client";
 
+import { useGlobalConfigStore } from "@/shared/libs/zustand/use-global-config-store";
+import { Button } from "@/shared/ui/button";
+import { motionVars } from "@/widgets/sidebar/model/motion";
+
+import { Search, SquarePen } from "lucide-react";
+import { AnimatePresence, motion } from "motion/react";
+
 export const Nav = () => {
-  return <div></div>;
+  const { isSidebarOpen } = useGlobalConfigStore();
+
+  return (
+    <div className="">
+      {["New chat", "Search chats"].map((item, i) => {
+        return (
+          <Button
+            key={item}
+            variant="ghost"
+            className="flex items-center gap-1 *:stroke-[1.5] [&>svg]:size-5"
+          >
+            {i === 0 ? <SquarePen /> : <Search />}
+            <AnimatePresence>
+              {isSidebarOpen && (
+                <motion.span
+                  variants={motionVars}
+                  initial="hidden"
+                  animate="visible"
+                  exit="hidden"
+                >
+                  {item}
+                </motion.span>
+              )}
+            </AnimatePresence>
+          </Button>
+        );
+      })}
+    </div>
+  );
 };

6d4d31633ed2c04454261fcf741974561d856218
Choi Chan Hwan <africakokiri@gmail.com>
2026-01-07 17:12:26 +0900

    feat: sidebar 여닫는 버튼 추가
    - button은 header 컴포넌트 내에 위치

--- src/widgets/sidebar/ui/header.tsx
+++ src/widgets/sidebar/ui/header.tsx
@@ -1,10 +1,16 @@
 "use client";
 
+import { useGlobalConfigStore } from "@/shared/libs/zustand/use-global-config-store";
+import { Button } from "@/shared/ui/button";
+
+import { ChevronsLeft, ChevronsRight } from "lucide-react";
 import Image from "next/image";
 
 export const Header = () => {
+  const { isSidebarOpen, toggleSidebar } = useGlobalConfigStore();
+
   return (
-    <div>
+    <div className="flex items-center justify-between">
       <div className="flex items-center gap-1">
         <Image
           className="size-12"
@@ -13,8 +19,17 @@ export const Header = () => {
           width={48}
           height={48}
         />
+
         <span className="text-3xl font-light tracking-tight">WooZoo</span>
       </div>
+
+      <Button
+        variant="ghost"
+        className="size-12"
+        onClick={() => toggleSidebar()}
+      >
+        {isSidebarOpen ? <ChevronsLeft /> : <ChevronsRight />}
+      </Button>
     </div>
   );
 };

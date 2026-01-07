45ec6035c91579a3ffd7229ffcadd4ede80ec2dd
Choi Chan Hwan <africakokiri@gmail.com>
2026-01-07 19:17:44 +0900

    feat: prompt 컴포넌트 상호작용 버튼 추가
    - attach file, mic, submit

--- src/widgets/prompt/ui/prompt.tsx
+++ src/widgets/prompt/ui/prompt.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import { Button } from "@/shared/ui/button";
+
+import { ArrowUp, Mic, Plus } from "lucide-react";
 import Textarea from "react-textarea-autosize";
 
 export default function Prompt() {
   return (
     <form
-      className="flex rounded-3xl border border-neutral-500 transition-all duration-200
+      className="flex flex-col rounded-3xl border border-neutral-500 transition-all duration-200
 focus-within:shadow-2xl"
     >
       <Textarea
@@ -14,6 +17,28 @@ focus-within:shadow-2xl"
         maxRows={10}
         placeholder="Ask me anything"
       />
+
+      <div className="flex w-full justify-between px-4 pb-3 **:rounded-full">
+        <div className="flex gap-2">
+          <Button
+            variant="outline"
+            size="icon"
+          >
+            <Plus />
+          </Button>
+
+          <Button
+            variant="outline"
+            size="icon"
+          >
+            <Mic />
+          </Button>
+        </div>
+
+        <Button size="icon">
+          <ArrowUp />
+        </Button>
+      </div>
     </form>
   );
 }

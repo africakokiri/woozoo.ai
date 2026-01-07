20a5453e8ec5fb49d9d6d9707439225f344b95b4
Choi Chan Hwan <africakokiri@gmail.com>
2026-01-07 17:09:22 +0900

    feat: sidebar header 페이지 타이틀 추가
    - 이미지 및 WooZoo 텍스트

--- src/widgets/sidebar/ui/header.tsx
+++ src/widgets/sidebar/ui/header.tsx
@@ -1,9 +1,20 @@
 "use client";
 
+import Image from "next/image";
+
 export const Header = () => {
   return (
     <div>
-      <div></div>
+      <div className="flex items-center gap-1">
+        <Image
+          className="size-12"
+          src="/icons/main.svg"
+          alt="WooZoo main logo"
+          width={48}
+          height={48}
+        />
+        <span className="text-3xl font-light tracking-tight">WooZoo</span>
+      </div>
     </div>
   );
 };

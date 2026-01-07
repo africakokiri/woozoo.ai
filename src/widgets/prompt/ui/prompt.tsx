29821fe4c39ab1a0cc3937e5edd35ad2f534747f
Choi Chan Hwan <africakokiri@gmail.com>
2026-01-07 19:13:54 +0900

    feat: prompt 컴포넌트 textarea 추가
    - 기능 구현 X

--- src/widgets/prompt/ui/prompt.tsx
+++ src/widgets/prompt/ui/prompt.tsx
@@ -1,5 +1,19 @@
 "use client";
 
+import Textarea from "react-textarea-autosize";
+
 export default function Prompt() {
-  return <form></form>;
+  return (
+    <form
+      className="flex rounded-3xl border border-neutral-500 transition-all duration-200
+focus-within:shadow-2xl"
+    >
+      <Textarea
+        className="w-full resize-none rounded-2xl px-4 py-3 outline-none"
+        minRows={2}
+        maxRows={10}
+        placeholder="Ask me anything"
+      />
+    </form>
+  );
 }

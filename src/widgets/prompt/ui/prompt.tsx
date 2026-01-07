9ba65a653ef1298eb08823a08c2e3e5a0a8df2ca
Choi Chan Hwan <africakokiri@gmail.com>
2026-01-07 19:26:47 +0900

    feat: prompt onKeyDown 분기 처리
    - enter를 누를 때와 enter + shift 구분

--- src/widgets/prompt/ui/prompt.tsx
+++ src/widgets/prompt/ui/prompt.tsx
@@ -3,11 +3,12 @@
 import { Button } from "@/shared/ui/button";
 
 import { ArrowUp, Mic, Plus } from "lucide-react";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Textarea from "react-textarea-autosize";
 
 export default function Prompt() {
   const [prompt, setPrompt] = useState("");
+  const formRef = useRef<HTMLFormElement>(null);
 
   const validPrompt = prompt.trim().length > 0;
 
@@ -15,13 +16,23 @@ export default function Prompt() {
     <form
       className="flex flex-col rounded-3xl border border-neutral-500 transition-all duration-200
 focus-within:shadow-2xl"
+      ref={formRef}
     >
       <Textarea
         className="w-full resize-none rounded-2xl px-4 py-3 outline-none"
         minRows={2}
         maxRows={10}
+        autoFocus
         placeholder="Ask me anything"
         onChange={(e) => setPrompt(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            if (e.shiftKey) return;
+            if (e.nativeEvent.isComposing) return;
+
+            formRef.current?.requestSubmit();
+          }
+        }}
       />
 
       <div className="flex w-full justify-between px-4 pb-3 **:rounded-full">

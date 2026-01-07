16a79ae38972dea09fe77b655203af52b95472db
Choi Chan Hwan <africakokiri@gmail.com>
2026-01-07 16:59:54 +0900

    style: SIDEBAR_WIDTH 상수 추가
    - MIN_SIDEBAR_WIDHT = 81px
    - MAX_SIDEBAR_WIDTH = 289px

--- src/widgets/sidebar/ui/sidebar.tsx
+++ src/widgets/sidebar/ui/sidebar.tsx
@@ -2,15 +2,23 @@
 
 import { motion } from "motion/react";
 
+const MIN_SIDEBAR_WIDTH = "81px"; // 5rem + 1px(border)
+const MAX_SIDEBAR_WIDTH = "289px"; // 18rem + 1px(border)
+
 export default function Sidebar() {
-  <motion.aside>
-    {/* 페이지 로고 */}
-    <header></header>
+  return (
+    <motion.aside
+      initial={false}
+      className="bg-sidebar border-r"
+    >
+      {/* 페이지 로고 */}
+      <header></header>
 
-    {/* 새로운 채팅, 채팅 검색, 채팅 기록 등*/}
-    <nav></nav>
+      {/* 새로운 채팅, 채팅 검색, 채팅 기록 등*/}
+      <nav></nav>
 
-    {/* 프로필 */}
-    <footer></footer>
-  </motion.aside>;
+      {/* 프로필 */}
+      <footer></footer>
+    </motion.aside>
+  );
 }

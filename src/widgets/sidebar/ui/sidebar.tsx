fbd616f6151d5365d8bd52e35f97f95fdb095d7c
Choi Chan Hwan <africakokiri@gmail.com>
2026-01-07 16:48:36 +0900

    feat: sidebar 컴포넌트 레이아웃 구성 및 주석 추가
    - 구역 분리

--- src/widgets/sidebar/ui/sidebar.tsx
+++ src/widgets/sidebar/ui/sidebar.tsx
@@ -1,5 +1,16 @@
 "use client";
 
+import { motion } from "motion/react";
+
 export default function Sidebar() {
-  return null;
+  <motion.aside>
+    {/* 페이지 로고 */}
+    <header></header>
+
+    {/* 새로운 채팅, 채팅 검색, 채팅 기록 등*/}
+    <nav></nav>
+
+    {/* 프로필 */}
+    <footer></footer>
+  </motion.aside>;
 }

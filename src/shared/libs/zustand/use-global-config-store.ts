a119a80cd7c66e5f3db0649e588679ecbbb43dfc
Choi Chan Hwan <africakokiri@gmail.com>
2026-01-07 18:51:48 +0900

    feat: zustand hydration 감지 로직 추가
    - sidebar를 localStorage에서 불러오는 과정에서 hydration mismatch가 발생함에 따라 hydration 감지 로직을 추가

--- src/shared/libs/zustand/use-global-config-store.ts
+++ src/shared/libs/zustand/use-global-config-store.ts
@@ -4,17 +4,27 @@ import { createJSONStorage, persist } from "zustand/middleware";
 type GlobalConfig = {
   isSidebarOpen: boolean;
   toggleSidebar: () => void;
+
+  isHydrated: boolean;
+  setIsHydrated: () => void;
 };
 
 export const useGlobalConfigStore = create<GlobalConfig>()(
   persist(
     (set) => ({
       isSidebarOpen: true,
-      toggleSidebar: () => set((state) => ({ isSidebarOpen: !state.isSidebarOpen }))
+      toggleSidebar: () => set((state) => ({ isSidebarOpen: !state.isSidebarOpen })),
+
+      isHydrated: false,
+      setIsHydrated: () => set(() => ({ isHydrated: true }))
     }),
     {
       name: "is-sidebar-open",
-      storage: createJSONStorage(() => localStorage)
+      storage: createJSONStorage(() => localStorage),
+
+      onRehydrateStorage: () => (state) => {
+        state?.setIsHydrated();
+      }
     }
   )
 );

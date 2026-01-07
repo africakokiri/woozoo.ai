5ecc8947764da1dd3e11630879215d1c41410549
Choi Chan Hwan <africakokiri@gmail.com>
2026-01-07 18:37:05 +0900

    feat: sidebar nav 컴포넌트 적용
    - + 버튼 또는 New chat 클릭 시 / 라우트로 이동

--- src/widgets/sidebar/ui/nav.tsx
+++ src/widgets/sidebar/ui/nav.tsx
@@ -1,40 +1,25 @@
 "use client";
 
+import { cn } from "@/shared/libs/tw/cn";
 import { useGlobalConfigStore } from "@/shared/libs/zustand/use-global-config-store";
 import { Button } from "@/shared/ui/button";
-import { motionVars } from "@/widgets/sidebar/model/motion";
 
-import { Search, SquarePen } from "lucide-react";
-import { AnimatePresence, motion } from "motion/react";
+import { Plus } from "lucide-react";
+import Link from "next/link";
 
 export const Nav = () => {
   const { isSidebarOpen } = useGlobalConfigStore();
 
   return (
-    <div className="">
-      {["New chat", "Search chats"].map((item, i) => {
-        return (
-          <Button
-            key={item}
-            variant="ghost"
-            className="flex items-center gap-1 *:stroke-[1.5] [&>svg]:size-5"
-          >
-            {i === 0 ? <SquarePen /> : <Search />}
-            <AnimatePresence>
-              {isSidebarOpen && (
-                <motion.span
-                  variants={motionVars}
-                  initial="hidden"
-                  animate="visible"
-                  exit="hidden"
-                >
-                  {item}
-                </motion.span>
-              )}
-            </AnimatePresence>
-          </Button>
-        );
-      })}
+    <div className="space-y-2">
+      <Button
+        variant={isSidebarOpen ? "default" : "ghost"}
+        className={cn("w-full", !isSidebarOpen && "h-12")}
+        size="sm"
+        asChild
+      >
+        <Link href="/">{isSidebarOpen ? <span>New chat</span> : <Plus />}</Link>
+      </Button>
     </div>
   );
 };
